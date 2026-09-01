4f5e24f13144f0bbc0f29c96f03c2d03d2bbeea2
Rodrigo Sambade <43375046+rodrigosambadesaa@users.noreply.github.com>
2026-09-01 12:52:02 +0200

    fix: publish installable APK release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "dev.marcosfarias.pokedex"
         minSdk = 23
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "3.0.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
